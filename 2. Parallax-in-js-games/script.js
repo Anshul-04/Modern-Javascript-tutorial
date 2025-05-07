ebdf46a969f4c3a545311a2b680f15217d024851
Anshul-04 <anshulonmars@gmail.com>
2025-05-08 03:27:16 +0530

    Endlessly scrolling bg technique update

--- 2. Parallax-in-js-games/script.js
+++ 2. Parallax-in-js-games/script.js
@@ -3,6 +3,7 @@
  */
 const canvas = document.getElementById("canvas");
 const ctx = canvas.getContext("2d");
+// let gameFrame = 0;
 
 /** 
  * Canvas dimensions.
@@ -29,70 +30,72 @@ backgroundLayer4.src = "assets/layer-4.png";
 const backgroundLayer5 = new Image();
 backgroundLayer5.src = "assets/layer-5.png";
 
-/**
- * Slider for adjusting game speed.
- */
-const slider = document.getElementById("slider");
-slider.value = gameSpeed;
-const showGameSpeed=  document.getElementById("showGameSpeed");
-showGameSpeed.innerHTML = gameSpeed;
-slider.addEventListener("input", function(e){
-  gameSpeed=e.target.value;
+window.addEventListener("load", function(){
+
+  /**
+  * Slider for adjusting game speed.
+  */
+  const slider = document.getElementById("slider");
+  slider.value = gameSpeed;
+  const showGameSpeed=  document.getElementById("showGameSpeed");
   showGameSpeed.innerHTML = gameSpeed;
-})
-
-/**
- * Layer class for managing background layers.
- */
-class Layer{
-  constructor(image,speedModifier){
-    this.x = 0;
-    this.y = 0;
-    this.width = 2400;
-    this.height = 700 ;
-    this.x2 = this.width;
-    this.image = image;
-    this.speedModifier = speedModifier;
-    this.speed = gameSpeed * this.speedModifier;
-  }
-
-  update(){
-    this.speed = gameSpeed * this.speedModifier;
-    if(this.x <=-this.width){
-      this.x = this.width + this.x2 - this.speed;
-    }
-    if(this.x2 <=-this.width){
-      this.x2 = this.width + this.x - this.speed;
-    }
-    this.x = Math.floor(this.x - this.speed);
-    this.x2 = Math.floor(this.x2 - this.speed);
-
-  }
-
-  draw(){
-    ctx.drawImage(this.image, this.x ,this.y , this.width, this.height);
-    ctx.drawImage(this.image, this.x2 ,this.y , this.width, this.height);
-  }
-}
-
-const layer1 = new Layer(backgroundLayer1, 0.2);
-const layer2 = new Layer(backgroundLayer2, 0.4);
-const layer3 = new Layer(backgroundLayer3, 0.6);
-const layer4 = new Layer(backgroundLayer4, 0.8);
-const layer5 = new Layer(backgroundLayer5, 1);
-
-const gameObjects = [layer1, layer2, layer3, layer4, layer5];
-
-function animate(){
-  ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
-  gameObjects.forEach( object =>{
-    object.update();
-    object.draw();
+  slider.addEventListener("input", function(e){
+    gameSpeed=e.target.value;
+    showGameSpeed.innerHTML = gameSpeed;
   })
 
-  requestAnimationFrame(animate);
-}
-animate();
+  /**
+  * Layer class for managing background layers.
+  */
+  class Layer{
+    constructor(image,speedModifier){
+      this.x = 0;
+      this.y = 0;
+      this.width = 2400;
+      this.height = 700 ;
+      this.image = image;
+      this.speedModifier = speedModifier;
+      this.speed = gameSpeed * this.speedModifier;
+    }
+
+    update(){
+      this.speed = gameSpeed * this.speedModifier;
+      if(this.x <=-this.width){
+        this.x =0;
+      }
+      
+      this.x = this.x - this.speed;
+      // this.x = gameFrame * this.speed % this.width;
+
+    }
+
+    draw(){
+      ctx.drawImage(this.image, this.x ,this.y , this.width, this.height);
+      ctx.drawImage(this.image, this.x + this.width ,this.y , this.width, this.height);
+    }
+  }
+
+  const layer1 = new Layer(backgroundLayer1, 0.2);
+  const layer2 = new Layer(backgroundLayer2, 0.4);
+  const layer3 = new Layer(backgroundLayer3, 0.6);
+  const layer4 = new Layer(backgroundLayer4, 0.8);
+  const layer5 = new Layer(backgroundLayer5, 1);
+
+  const gameObjects = [layer1, layer2, layer3, layer4, layer5];
+
+  function animate(){
+    ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+    gameObjects.forEach( object =>{
+      object.update();
+      object.draw();
+    });
+
+    // gameFrame--;
+    requestAnimationFrame(animate);
+  }
+  animate();
+});
+
 
 /**
  * Parallax Effect is when Foreground layers move 
