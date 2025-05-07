5da742063d3dbc87eaa2265b6ef12f5d911e592e
Anshul-04 <anshulonmars@gmail.com>
2025-05-08 02:43:02 +0530

    Using Class for different background Layer

--- 2. Parallax-in-js-games/script.js
+++ 2. Parallax-in-js-games/script.js
@@ -13,7 +13,7 @@ const CANVAS_HEIGHT = canvas.height = 700;
 /** 
  * Game speed for background scrolling.
  */
-let gameSpeed = 15;
+let gameSpeed = 5; // Speed of the background layers
 
 /**
  * Background layer images.
@@ -29,19 +29,52 @@ backgroundLayer4.src = "assets/layer-4.png";
 const backgroundLayer5 = new Image();
 backgroundLayer5.src = "assets/layer-5.png";
 
-let x = 0; // First layer position
-let x2 = 2400; // Second layer x position for parallax scrolling
+class Layer{
+  constructor(image,speedModifier){
+    this.x = 0;
+    this.y = 0;
+    this.width = 2400;
+    this.height = 700 ;
+    this.x2 = this.width;
+    this.image = image;
+    this.speedModifier = speedModifier;
+    this.speed = gameSpeed * this.speedModifier;
+  }
+
+  update(){
+    this.speed = gameSpeed * this.speedModifier;
+    if(this.x <=-this.width){
+      this.x = this.width + this.x2 - this.speed;
+    }
+    if(this.x2 <=-this.width){
+      this.x2 = this.width + this.x - this.speed;
+    }
+    this.x = Math.floor(this.x - this.speed);
+    this.x2 = Math.floor(this.x2 - this.speed);
+
+  }
+
+  draw(){
+    ctx.drawImage(this.image, this.x ,this.y , this.width, this.height);
+    ctx.drawImage(this.image, this.x2 ,this.y , this.width, this.height);
+  }
+}
+
+const layer1 = new Layer(backgroundLayer1, 0.2);
+const layer2 = new Layer(backgroundLayer2, 0.4);
+const layer3 = new Layer(backgroundLayer3, 0.6);
+const layer4 = new Layer(backgroundLayer4, 0.8);
+const layer5 = new Layer(backgroundLayer5, 1);
+
+const gameObjects = [layer1, layer2, layer3, layer4, layer5];
 
 function animate(){
   ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
-  ctx.drawImage(backgroundLayer4, x, 0);
-  ctx.drawImage(backgroundLayer4, x2, 0);
+  gameObjects.forEach( object =>{
+    object.update();
+    object.draw();
+  })
 
-  if(x < -2400) x = 2400 + x2- gameSpeed; 
-  else x -= gameSpeed;
-  if(x2 < -2400) x2 = 2400 + x - gameSpeed; 
-  else x2 -= gameSpeed ; // Slower speed for the second layer
-  
   requestAnimationFrame(animate);
 }
 animate();
