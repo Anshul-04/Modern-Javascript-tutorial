2309549cd9ce40900d85d4db0b4f2be2f13faa18
Anshul-04 <anshulonmars@gmail.com>
2025-05-08 16:33:15 +0530

    created Enemy Class and implemented many enemies objects

--- 3. Coding-flying-creatures-with-js/script.js
+++ 3. Coding-flying-creatures-with-js/script.js
@@ -5,19 +5,45 @@ const ctx = canvas.getContext('2d');
 
 const CANVAS_WIDTH = canvas.width = 600;
 const CANVAS_HEIGHT = canvas.height = 1000;
+const numberOfEnemies = 50;
+const enemiesArrays = [];
 
-enemy1 ={
-  x: 10,
-  y: 20,
-  width: 100,
-  height: 100
+// enemy1 ={
+//   x: 10,
+//   y: 20,
+//   width: 100,
+//   height: 100
+// }
+
+class Enemy{
+  constructor(){
+    this.x = Math.random() * canvas.width;
+    this.y = Math.random() * canvas.height;
+    this.width = 100;
+    this.height = 100;
+    this.speed = Math.random() * 4 - 2; //range between -2 and 2
+  }
+
+  update(){
+    this.x+= this.speed;
+    this.y+= this.speed;
+  }
+
+  draw(){
+    ctx.fillRect(this.x ,this.y , this.width , this.height);
+  }
 }
+
+for(let i=0; i<numberOfEnemies; i++){
+  enemiesArrays.push(new Enemy());  
+} 
 
 function animate(){
   ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
-  enemy1.x++;
-  enemy1.y++;
-  ctx.fillRect(enemy1.x ,enemy1.y , enemy1.width , enemy1.height);
+  enemiesArrays.forEach( enemy =>{
+    enemy.update();
+    enemy.draw();
+  })
   requestAnimationFrame(animate);
 }
 animate();
