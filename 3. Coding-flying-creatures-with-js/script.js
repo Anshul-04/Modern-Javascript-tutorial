16a3d4f9a4761954e23630d368e0931e1e65e2ef
Anshul-04 <anshulonmars@gmail.com>
2025-05-08 17:12:55 +0530

    Random Chaotic Jumps

--- 3. Coding-flying-creatures-with-js/script.js
+++ 3. Coding-flying-creatures-with-js/script.js
@@ -7,30 +7,41 @@ const CANVAS_WIDTH = canvas.width = 600;
 const CANVAS_HEIGHT = canvas.height = 1000;
 const numberOfEnemies = 50;
 const enemiesArrays = [];
+let gameFrame = 0;
 
-// enemy1 ={
-//   x: 10,
-//   y: 20,
-//   width: 100,
-//   height: 100
-// }
+
 
 class Enemy{
   constructor(){
-    this.x = Math.random() * canvas.width;
-    this.y = Math.random() * canvas.height;
-    this.width = 100;
-    this.height = 100;
-    this.speed = Math.random() * 4 - 2; //range between -2 and 2
+    this.image = new Image();
+    this.image.src = "assets/enemy1.png";      
+    // this.speed = Math.random() * 4 - 2; //range between -2 and 2
+    this.spriteWidth = 293;
+    this.spriteHeight = 155;
+    this.width = this.spriteWidth / 2.5 ;
+    this.height = this.spriteHeight / 2.5 ;
+    this.x = Math.random() * (canvas.width - this.width);
+    this.y = Math.random() * (canvas.height - this.height);
+    this.frame = 0 ;
+    this.flapSpeed = Math.floor(Math.random() * 3 + 1 ); //random number between 1 and 3
+    
   }
 
   update(){
-    this.x+= this.speed;
-    this.y+= this.speed;
+    this.x+= Math.random() * 8 - 4; //range between -2 and 2
+    this.y+= Math.random() * 8 - 4; //range between -2 and 2
+    
+    //animate sprites
+    if(gameFrame % this.flapSpeed === 0){
+      this.frame > 4 ? this.frame = 0 : this.frame++ ;
+    }
+
   }
 
   draw(){
-    ctx.fillRect(this.x ,this.y , this.width , this.height);
+    ctx.drawImage(this.image, this.frame * this.spriteWidth , 0,
+      this.spriteWidth, this.spriteHeight , 
+      this.x , this.y , this.width , this.height,);
   }
 }
 
@@ -43,7 +54,8 @@ function animate(){
   enemiesArrays.forEach( enemy =>{
     enemy.update();
     enemy.draw();
-  })
+  });
+  gameFrame++;
   requestAnimationFrame(animate);
 }
 animate();
