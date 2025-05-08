e9630fc4e4a553e1a8286f507ab8992220c36831
Anshul-04 <anshulonmars@gmail.com>
2025-05-08 16:08:13 +0530

    animate single Object in canva

--- 3. Coding-flying-creatures-with-js/script.js
+++ 3. Coding-flying-creatures-with-js/script.js
@@ -3,5 +3,21 @@
 const canvas = document.getElementById('canvas');
 const ctx = canvas.getContext('2d');
 
-const CANVAS_WIDTH = canvas.width = 500;
-const CANVAS_HEIGHT = canvas.height = 900;
+const CANVAS_WIDTH = canvas.width = 600;
+const CANVAS_HEIGHT = canvas.height = 1000;
+
+enemy1 ={
+  x: 10,
+  y: 20,
+  width: 100,
+  height: 100
+}
+
+function animate(){
+  ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+  enemy1.x++;
+  enemy1.y++;
+  ctx.fillRect(enemy1.x ,enemy1.y , enemy1.width , enemy1.height);
+  requestAnimationFrame(animate);
+}
+animate();
