c354ff211273b83ce91851c08ad16c3c759c28f5
Anshul-04 <anshulonmars@gmail.com>
2025-05-10 21:13:04 +0530

    Setup HTML canva project

---
+++ 4. Audio-Visual-effect-JS/script.js
@@ -0,0 +1,8 @@
+const canvas = document.getElementById("canvas1");
+const ctx = canvas.getContext("2d");
+
+canvas.width = 600;
+canvas.height = 600;
+
+ctx.fillStyle = "red";
+ctx.fillRect(50,50, 100, 100);
